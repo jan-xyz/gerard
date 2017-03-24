34d79a5e37f772a6a295183903be71a4a5ff3a40
Jan Steinke <jan.steinke@gmail.com>
2017-03-24 16:58:15 +0100

    connect to the Slack servers

--- first_test.go
+++ first_test.go
@@ -1,14 +1,24 @@
 package gerard
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func TestConnect(t *testing.T) {
-	Connect()
-}
+//func TestConnect(t *testing.T) {
+//	Connect()
+//}
 
 func TestLoginToSlack(t *testing.T) {
-	test := LoginToSlack()
-	if test != "ws://echo.websocket.org/" {
+	GetWssURL()
+}
+
+func TestGetLoginURL(t *testing.T) {
+	testAPIToken := "test"
+	expectedURL := "http://slack.com/api/rtm.start?token=" + testAPIToken
+	os.Setenv("ROLLMOPS_SLACK_API_KEY", testAPIToken)
+	url := getLoginURL()
+	if url != expectedURL {
 		t.Fail()
 	}
 }
--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package gerard
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 
 	"golang.org/x/net/websocket"
 )
@@ -11,16 +15,18 @@ func main() {
 	Connect()
 }
 
+// HTTPSlackResponse : structure that should fit the JSON getting back
+type HTTPSlackResponse struct {
+	URL string `json:"url"`
+}
+
 // Connect : connects to a websocket
 func Connect() {
 	origin := "http://localhost/"
-	url := LoginToSlack()
+	url := GetWssURL()
 	ws, err := websocket.Dial(url, "", origin)
 	if err != nil {
 		log.Fatal(err)
-	}
-	if _, newerr := ws.Write([]byte("hello, world!\n")); err != nil {
-		log.Fatal(newerr)
 	}
 	var msg = make([]byte, 512)
 	var n int
@@ -30,7 +36,37 @@ func Connect() {
 	fmt.Printf("Received: %s.\n", msg[:n])
 }
 
-// LoginToSlack : returns a wss URL for Slack
-func LoginToSlack() string {
-	return "ws://echo.websocket.org/"
+// GetWssURL : returns a wss URL for Slack
+func GetWssURL() string {
+	loginURL := getLoginURL()
+	reader := getLoginRequestReader(loginURL)
+	LoginJSON := getLoginJSONFromReader(reader)
+	closeLoginRequestReader(reader)
+	return LoginJSON.URL
+}
+
+func getLoginURL() string {
+	apikey := os.Getenv("ROLLMOPS_SLACK_API_KEY")
+	return "http://slack.com/api/rtm.start?token=" + apikey
+}
+
+func getLoginRequestReader(loginURL string) io.ReadCloser {
+	resp, err := http.Get(loginURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return resp.Body
+}
+
+func getLoginJSONFromReader(reader io.ReadCloser) *HTTPSlackResponse {
+	m := new(HTTPSlackResponse)
+	err := json.NewDecoder(reader).Decode(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return m
+}
+
+func closeLoginRequestReader(reader io.ReadCloser) {
+	defer reader.Close()
 }
