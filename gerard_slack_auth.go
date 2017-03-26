56d70f05163cb8eb574ba287a83afee0853591a3
Jan Steinke <jan.steinke@gmail.com>
2017-03-26 16:08:38 +0200

    expand JSON parsed from HTTP authentication

--- gerard_slack_auth.go
+++ gerard_slack_auth.go
@@ -9,6 +9,36 @@ import (
 	"os"
 )
 
+// HTTPSlackResponse : structure that should fit the JSON getting back
+type HTTPSlackResponse struct {
+	URL      string `json:"url"`
+	OK       bool   `json:"ok"`
+	Users    []user `json:"users"`
+	Teams    team   `json:"team"`
+	Channels []team `json:"channels"`
+	Groups   []team `json:"groups"`
+}
+
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type team struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type channel struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+type group struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
 // GetWssURL : returns a wss URL for Slack
 func GetWssURL() string {
 	loginURL := getLoginURL()
