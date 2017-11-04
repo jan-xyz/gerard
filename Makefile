031851fc7530a80fb13456246f4ec6cee8a12973
Jan Steinke <jan.steinke@gmail.com>
2017-11-04 12:30:36 +0100

    move test out of the test folder into the package

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@ compile: deps test
 	go build ./cmd/gerardd
 
 test: deps
-	go test ./gerard-core/tests/
+	go test ./gerard-core/
 
 deps:
 	go get -u github.com/golang/dep/cmd/dep
--- gerard-core/slack_websocket_test.go
+++ gerard-core/slack_websocket_test.go
@@ -1,16 +1,13 @@
-package tests
+package gerard_core
 
 import (
-	"os"
 	"testing"
-	"github.com/jan-xyz/gerard/gerard-core"
 )
 
 func TestGetLoginURL(t *testing.T) {
 	testAPIToken := "test"
 	expectedURL := "http://slack.com/api/rtm.start?token=" + testAPIToken
-	os.Setenv("ROLLMOPS_SLACK_API_KEY", testAPIToken)
-	url := gerard_core.GetLoginURL()
+	url := GetLoginURL(testAPIToken)
 	if url != expectedURL {
 		t.Fail()
 	}
