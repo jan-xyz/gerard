fb8cc225c1b041c7407df1d3303d23844a96f23b
Jan Steinke <jan.steinke@gmail.com>
2020-06-05 17:54:09 +0200

    remove redundant type

--- gerard_slack_websocket.go
+++ gerard_slack_websocket.go
@@ -8,7 +8,7 @@ import (
 
 func connectWebsocket(slackURL string) *websocket.Conn {
 	origin := "http://localhost/"
-	headers := http.Header(map[string][]string{"origin": []string{origin}})
+	headers := http.Header(map[string][]string{"origin": {origin}})
 	conn, resp, err := websocket.DefaultDialer.Dial(slackURL, headers)
 	if resp.StatusCode != http.StatusSwitchingProtocols {
 		log.Fatalf("response from slack not ok: %d", resp.StatusCode)
